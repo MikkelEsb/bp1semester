429c4c1093960593fc783308073b96460a4a6f3c
mspruc <55182629+mspruc@users.noreply.github.com>
2019-11-04 09:48:37 +0100

    Add files via upload
    progress

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,25 +1,45 @@
 import java.util.ArrayList;
 
 public class Statistics {
-    ArrayList<Float> vectorlength;
+    ArrayList<Float> vectorLengths;
     int average;
-    int i;
+    int i = 1;
     float x;
+    int t = 60; //how much data stored in array measured in time interval
+    int N;      //define this as the number of vehicles in the system, this probably to remain constant
+    ArrayList<Integer> movingAverage;
 
-    public Statistics(){vectorlength = new ArrayList<Float>(60);
+    //Constructs statistics as an array of floats that should be equal to the length of the vector of whatever vehicle
+    public Statistics(){
+        vectorLengths = new ArrayList<Float>(t*N);
+        movingAverage = new ArrayList<>();
     }
+
     //add your stats to the arraylist using the following command
-    void addStats(float x){
-        vectorlength.add(x);
+    public void addStats(float x) {
+        if (vectorLengths.size() > t * N) {
+            vectorLengths.add(x);
+
+        } else {
+            vectorLengths.set(i,x);
+            i = i++%60;
+
+            if(i == 59){
+                movingAverage.add(average());
+            }
+        }
     }
     //prints your average
-    int average(){
-        for(int i = 0; i < vectorlength.size(); i++){
-            x = x + vectorlength.get(i);                //sums all the lengths and assigns them to memory at x.
+    public int average(){
+        for(int i = 0; i < vectorLengths.size(); i++) {
+            x = x + vectorLengths.get(i);                //sums all the lengths and assigns them to memory at x.
         }
-        average = (int) (x / vectorlength.size());      //calculates the average according to the size of your data, time interval should matter somewhere
+        average = (int) (x / vectorLengths.size());      //calculates the average according to the size of your data, time interval should matter somewhere
         return(average);
     }
-    //sanity control
-    public ArrayList<Float> getStatistics(){return vectorlength;}
+
+    //return functions
+    public ArrayList<Float> getStatistics(){return vectorLengths;}
+    public ArrayList<Integer> getMovingAverage(){return movingAverage;}
+    public float currentAverage(){return average;}
 }
