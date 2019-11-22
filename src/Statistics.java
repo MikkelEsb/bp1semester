f3b090d87169a43300d73d11f83071e7823dbd55
mspruc <55182629+mspruc@users.noreply.github.com>
2019-11-22 09:58:53 +0100

    Add files via upload

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,46 +1,72 @@
 import java.util.ArrayList;
 
 class Statistics {
-    ArrayList<Float> vectorLengths;
-    int average;
-    int i = 1;
-    float x;
-    int t = 60; //how much data stored in array measured in time interval
-    ArrayList<Integer> movingAverage;
-    int N;
+    private ArrayList<Integer> movingAverageSpeed;
+    private ArrayList<Integer> movingAverageDensity;
+    private ArrayList<Integer> movingAverageFlow;
+    private ArrayList<Float> vectorLengths;
+    private int averageSpeed;
+    private int averageFlow;
+    private int averageDensity;
+    private int t = 60; //how much data stored in array measured in time interval
+    private int j = 0;
+    private int N;
 
     //Constructs statistics as an array of floats that should be equal to the length of the vector of whatever vehicle
-    Statistics(int n){
-        vectorLengths = new ArrayList<Float>(t*n);
-        movingAverage = new ArrayList<>();
-        N = n;
+    Statistics(int carCount){
+        N = carCount;
+        vectorLengths        = new ArrayList<Float>(t*N);
+        movingAverageSpeed   = new ArrayList<Integer>();
+        movingAverageDensity = new ArrayList<Integer>();
+        movingAverageFlow    = new ArrayList<Integer>();
     }
 
     //add your stats to the arraylist using the following command
-    public void addStats(float x) {
-        if (vectorLengths.size() > t * N) {
-            vectorLengths.add(x);
+    public void addStats(float speed) {
+        if (vectorLengths.size() < t * N) {
+            vectorLengths.add(speed);
 
         } else {
-            vectorLengths.set(i,x);
-            i = i++%60;
+            vectorLengths.set(j,speed);
+            j = j++%60;
 
-            if(i == 59){
-                movingAverage.add(average());
+            if(j == 59){
+                movingAverageSpeed.add(averageSpeed());
+                movingAverageDensity.add(averageDensity());
+                movingAverageFlow.add(averageFlow());
             }
         }
     }
+
     //prints your average
-    public int average(){
-        for(int i = 0; i < vectorLengths.size(); i++) {
-            x = x + vectorLengths.get(i);                //sums all the lengths and assigns them to memory at x.
+    private int averageSpeed(){
+        float x = 0;
+        for(int i = 0; i == vectorLengths.size(); i++) {
+            x = x + vectorLengths.get(i);                //sums the lengths of all vehicles and assigns them to memory at x.
         }
-        average = (int) (x / vectorLengths.size());      //calculates the average according to the size of your data, time interval should matter somewhere
-        return(average);
+        averageSpeed = (int) (x / vectorLengths.size());      //calculates the average according to the size of your data, time interval should matter somewhere
+        return(averageSpeed);
+    }
+
+    //prints average density for the iteration, is calculated from road start to road end, to intersection.
+    private int averageDensity(){
+        for(int i = 0; i < vectorLengths.size(); i++) {
+
+        }
+        return(averageDensity);
+    }
+
+    //prints average flow for the iteration
+    private int averageFlow(){
+        for(int i = 0; i < vectorLengths.size(); i++){
+            averageFlow = averageDensity * averageSpeed;
+        }
+        return(averageFlow);
     }
 
     //return functions
     public ArrayList<Float> getStatistics(){return vectorLengths;}
-    public ArrayList<Integer> getMovingAverage(){return movingAverage;}
-    public float currentAverage(){return average;}
+    public ArrayList<Integer> getMovingAverageSpeed(){return movingAverageSpeed;}
+    public ArrayList<Integer> getMovingAverageFlow(){return movingAverageFlow;}
+    public ArrayList<Integer> getMovingAverageDensity(){return movingAverageDensity;}
 }
